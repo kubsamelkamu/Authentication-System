5055ddfb829eb8b993dbf81c18f2d53692753bdc
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-12-12 22:49:17 +0300

    imported Route functionality

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const authRoutes = require('./routes/authRoutes');
 const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
 
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
     res.send('Authentication System is Running');
