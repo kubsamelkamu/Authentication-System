52626d9d3b6140d8405ff4ff4a8f0a4f8bc677e2
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-12-13 10:16:47 +0300

    register userRoutes

--- src/app.js
+++ src/app.js
@@ -3,18 +3,18 @@ const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
+const userRoutes = require('./routes/userRoute');
 
 
 dotenv.config();
-
 const app = express();
-
 
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes);
 
 app.get('/', (req, res) => {
     res.send('Authentication System is Running');
