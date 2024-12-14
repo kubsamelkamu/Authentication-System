0f1b053b51e32071526af29980aefb2d8eff0b3f
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-12-14 21:43:33 +0300

    register emailverificationRoutes

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const authRoutes = require('./routes/authRoutes');
 const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
 const userRoutes = require('./routes/userRoute');
+const emailVerificationRoutes = require("./routes/emailverificationRoute");
+
 
 
 dotenv.config();
@@ -15,6 +17,7 @@ app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
+app.use("/api/auth", emailVerificationRoutes);
 
 app.get('/', (req, res) => {
     res.send('Authentication System is Running');
