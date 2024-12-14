429d1ec1e52fcf3af1a9cc74cea95d4a76412ab2
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-12-14 21:41:30 +0300

    update the routes name

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const { register, loginUser, refreshToken, logoutUser } = require('../controller/authController');
+const { registerUser, loginUser,logoutUser } = require('../controller/authController');
 
 const router = express.Router();
 
-router.post('/register', register);
+router.post('/register',registerUser);
 router.post('/login', loginUser);
-router.post('/refresh-token', refreshToken);
 router.post('/logout', logoutUser);
 
 module.exports = router;
